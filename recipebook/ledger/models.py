e813fb0aa4fb8fc62bd108ca19376fd1b33fdcf6
loltreeman <naguio.greg@gmail.com>
2025-02-24 16:14:51 +0800

    Added related names for Recipe and Ingredient

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -18,11 +18,11 @@ class Ingredient(models.Model):
 class RecipeIngredient(models.Model):
     
     quantity = models.CharField(max_length=50)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE, related_name = "recipe")
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE, related_name = "ingredients")
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}"
     
     def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.recipe.pk)])
+        return reverse('recipe_detail', args = [str(self.recipe.pk)])
