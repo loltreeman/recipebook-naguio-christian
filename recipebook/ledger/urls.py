606fb3c4811c985fcc43688464c3e80cbde823ae
loltreeman <naguio.greg@gmail.com>
2025-02-28 13:33:36 +0800

    Added path for populating database

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('recipes/populate/', views.populateDatabase, name='populate_db'),
     path('recipes/list/', views.RecipeListView.as_view(), name="recipe_list"),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
 ]
