bf58641b309d82619ce798e219e5433d5d4d7f5c
loltreeman <naguio.greg@gmail.com>
2025-03-14 08:57:18 +0800

    Created Profile Model, and modified recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    name = models.TextField(max_length=50, blank=True)
+    user_bio = models.TextField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
