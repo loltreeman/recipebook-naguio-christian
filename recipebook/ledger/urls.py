4f4d2a2195f64ddb947673bb2d4f93d3b958f563
loltreeman <naguio.greg@gmail.com>
2025-03-18 12:24:53 +0800

    Added homepage path for login view, and logout path for logout view

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path('', LoginView.as_view(template_name='registration/login.html'), name="login"),  
+    path('logout/', LogoutView.as_view(), name="logout"),  
     path('recipes/list/', views.RecipeListView.as_view(), name="recipe_list"),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
 ]
