838242db7d278cf2f8a0cdc1f74bc8a9dedb542c
loltreeman <naguio.greg@gmail.com>
2025-03-19 11:37:20 +0800

    Made it so that the author is now pointing to Profile instance

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    author = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    author = models.OneToOneField(Profile, on_delete=models.CASCADE, default=None)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
