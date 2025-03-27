2261cb897dfc72d47e1572a071fb6c59acf9f78b
loltreeman <naguio.greg@gmail.com>
2025-03-27 14:34:19 +0800

    8 - Added paths for adding recipes and adding images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),  
     path('recipes/list/', views.RecipeListView.as_view(), name="recipe_list"),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
+    path('recipes/add', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/add_image/', views.RecipeImageCreateView.as_view(), name='recipes_add_image'),
 ]
 
 app_name = "ledger"
