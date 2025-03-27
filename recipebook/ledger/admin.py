cfe80d18f504e86c75aed608682f1b2e818f538a
loltreeman <naguio.greg@gmail.com>
2025-03-27 14:53:16 +0800

    11 - Added inline tables for Recipe Images an inserted them in Recipe Admin too

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImageInline
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 # Register your models here.
 
@@ -12,32 +12,36 @@ class ProfileInline(admin.StackedInline):
 
 class CustomUserAdmin(UserAdmin):
     inlines = (ProfileInline,)
+
 class RecipeIngredientInline(admin.TabularInline):
-    
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
 class RecipeAdmin(admin.ModelAdmin):
-    
     model = Recipe
     list_display = ('name', ) 
     search_fields = ('name', )  
     inlines = [RecipeIngredientInline, RecipeImageInline]
 
 class IngredientAdmin(admin.ModelAdmin):
-    
     model = Ingredient 
     list_display = ('name', )
     search_fields = ('name',  )
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
-    
     model = RecipeIngredient
     list_display = ('recipe', 'ingredient', 'quantity')  
     list_filter = ('recipe', 'ingredient')  
     search_fields = ('recipe__name', 'ingredient__name') 
+
+class RecipeImageAdmin(admin.ModelAdmin):
+    model = RecipeImage
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(RecipeImage, RecipeImageAdmin)
