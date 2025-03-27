cb83762d7fed78903677e671a8a7adbe4b78749d
loltreeman <naguio.greg@gmail.com>
2025-03-27 14:37:37 +0800

    9 - Moved the RecipeImage model down the other functions for Recipe to be defined

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,16 +3,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
-class RecipeImage(models.Model):
-    image = models.ImageField(upload_to='recipe_images/', null=False)
-    description = models.CharField(max_length=255)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='images')
-
-    def __str__(self):
-        return f"Image for {self.recipe.name}"
-
-    def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.recipe.pk)])
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
@@ -24,12 +14,26 @@ class Profile(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    author = models.OneToOneField(Profile, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', kwargs={'pk': self.pk})
+    
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='recipe_images/', null=False)
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return f"Image for {self.recipe.name}"
+
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.recipe.pk)])
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
