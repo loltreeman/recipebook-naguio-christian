77856daa492215ba819c6760a4a019a8993f0764
loltreeman <naguio.greg@gmail.com>
2025-03-27 15:10:32 +0800

    13 - Did the bonus where you hide the secret key in settings.py

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-caqu@dwoj=7unc@))&5#gdy^fy)s)y=qj02r%rjt6tc3rwl794'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+if not DEBUG:
+    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'ledger:recipe_list'
+
+LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
