42e8e06b068ba3e76e08ae57f86289ebd65ac4bf
loltreeman <naguio.greg@gmail.com>
2025-04-06 20:36:45 +0800

    Added more models to import for forms

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Recipe, RecipeImage
+from .models import Recipe, RecipeImage, Ingredient, RecipeIngredient
  
 class RecipeForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,14 @@ class RecipeImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
         fields = ['image', 'description']
+
+class RecipeIngredientForm(forms.ModelForm):
+    
+    class Meta:
+        model = RecipeIngredient
+        fields = ['ingredient', 'quantity']
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
